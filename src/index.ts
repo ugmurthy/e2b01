b5d07c016283f1b6f3d937a250600de35a903238
Murthy Udupa <ugmurthy@gmail.com>
2025-05-01 13:31:07 -0700

    getting started with e2b sandbox

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-console.log("New node env with tsc and ts-node");
-console.log("New node env with tsc and ts-node");
-console.log("New node env with tsc and ts-node");
+import 'dotenv/config'
+import { Sandbox } from '@e2b/code-interpreter'
+
+console.time('sandboxCreation')
+const sbx = await Sandbox.create() // By default the sandbox is alive for 5 minutes
+console.timeEnd('sandboxCreation')
+console.time('sandboxExecution')
+const execution = await sbx.runCode('print("hello world")') // Execute Python inside the sandbox
+const sh = await sbx.commands.run("ls -lut /bin")
+console.timeEnd('sandboxExecution')
+console.log(execution.logs)
+console.log(execution.results)
+console.log("------")
+console.log(sh.stdout)
+
+const files = await sbx.files.list('/')
+console.log(files)
 
